6af65e73cc9410243bebc3a4d2fee22b975b8113
olagunju-oluwabukola <olagunjuoluwabukola2018@gmail.com>
2026-03-10 22:16:17 +0100

    widget intro

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello'),
+          backgroundColor: Colors.brown[700],
+          centerTitle: true,
+        ),
+        body: const Home(),
+      ),
+    ),
+  );
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hey!I'm learning flutter.");
+  }
+}
